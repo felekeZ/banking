41142390e86b1580a1849b72a27b43cca740504d
felekeZ <terefefeleke@gmail.com>
2024-10-30 14:13:40 +0300

    implement the home, sidebar and mobile view layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 function Home() {
+  const loggedIn = {firstName: 'Feleke', lastName:'Zerihun', email:'feleke@gmail.com'}
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+           type="greeting"
+           title="Welcome"
+           user={loggedIn?.firstName || 'Guest'}
+           subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1230.5}
+          />
+        </header>
+        Recent Transactions
+      </div>
+      <RightSidebar 
+         user={loggedIn}
+         transactions={[]}
+         banks={[{currentBalance: 1250}, {currentBalance: 345}]}/>
+    </section>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Inter, IBM_Plex_Serif } from "next/font/google";
+export const dynamic = 'force-dynamic'
 
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const ibmPlexSerif = IBM_Plex_Serif({
@@ -9,7 +10,6 @@ const ibmPlexSerif = IBM_Plex_Serif({
   weight: ['400', '700'],
   variable: '--font-ibm-plex-serif'
 })
-
 
 export const metadata: Metadata = {
   title: "Kaya",
